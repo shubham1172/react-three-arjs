7dc1470fbed6b272df9e43ae558779beef272514
Shubham Sharma <shubhamsharma1172@gmail.com>
2022-04-30 12:04:02 +0530

    Use interfaces
    Signed-off-by: Shubham Sharma <shubhamsharma1172@gmail.com>

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,24 +1,31 @@
 import { Props } from '@react-three/fiber/dist/declarations/src/web/Canvas';
 
-export type ARCanvasProps = Props & {
-  arEnabled: boolean;
-  tracking: boolean;
-  patternRatio: number;
-  detectionMode: string;
-  cameraParametersUrl: string;
-  matrixCodeType: string;
-  sourceType: string;
-  onCameraStreamReady: () => void;
-  onCameraStreamError: () => void;
+export enum ARMarkerType {
+    Pattern,
+    BarCode,
+    NFT,
+    Unknown
 }
 
-export type ARMarkerProps = React.PropsWithChildren<string> & {
-  type: string;
-  barcodeValue: string;
-  patternUrl: string;
-  params: Object;
-  onMarkerFound: () => void;
-  onMarkerLost: () => void;
+export interface ARCanvasProps extends Props {
+    arEnabled?: boolean;
+    tracking?: boolean;
+    patternRatio?: number;
+    detectionMode?: string;
+    cameraParametersUrl?: string;
+    matrixCodeType?: string;
+    sourceType?: string;
+    onCameraStreamReady?: () => void;
+    onCameraStreamError?: () => void;
+}
+
+export interface ARMarkerProps extends Props {
+  type: ARMarkerType;
+  barcodeValue?: string;
+  patternUrl?: string;
+  params?: Object;
+  onMarkerFound?: () => void;
+  onMarkerLost?: () => void;
 }
 
 export declare function ARCanvas(props: ARCanvasProps): JSX.Element;
